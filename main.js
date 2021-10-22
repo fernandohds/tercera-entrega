12154d39565e974f68d944721b761c3995353041
fernandohds <fernandofigueroa.hds@gmail.com>
2021-10-21 23:49:17 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let edad = prompt ('Decime tu edad')
+
+if (edad < 30) {
+    alert("Sos menor que yo. Tengo 30")
+}
+else if (edad > 30) {
+    alert("Sos mayor que yo. Tengo 30")
+
+}
+else if (edad == 30) {
+    alert("Tenemos la misma edad")
+
+}
